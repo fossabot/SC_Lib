f664fb7fb83a116631b2aecf789fd1cd965683b9
danil <erofienkodanilka050@gmail.com>
2023-01-10 13:00:15 +0300

    Some update

--- app/src/main/java/com/samplecode/lib/java/async/AsyncActivityJ.java
+++ app/src/main/java/com/samplecode/lib/java/async/AsyncActivityJ.java
@@ -1,8 +1,15 @@
 package com.samplecode.lib.java.async;
 
+import android.os.Build;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.samplecode.lib.KotlinJavaExtensionsKt;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
 
 import kotlinx.coroutines.Job;
 
@@ -19,6 +26,24 @@ public abstract class AsyncActivityJ extends AppCompatActivity implements AsyncI
             }
         });
         jobs.add(job);
+    }
+
+    @SuppressWarnings({"unchecked", "deprecation"})
+    @Nullable
+    public <T extends Serializable> T getSerializeExtraOrNull(@NotNull String name, @NotNull Class<T> data) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            return this.getIntent().getSerializableExtra(name, data);
+        }
+        return (T) this.getIntent().getSerializableExtra(name);
+    }
+
+    @NotNull
+    public <T extends Serializable> T getSerializeExtra(@NotNull String name, @NotNull Class<T> data, @NotNull T defaultValue) {
+        T res = getSerializeExtraOrNull(name, data);
+        if (res != null) {
+            return res;
+        }
+        return defaultValue;
     }
 
     @Override
